05fb7991a5763f3e9aa9ff093b7066bae6a01213
csaEsplai <carloslecun@gmail.com>
2022-07-14 02:18:47 +0200

    Inicio

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program { 
+        static void Main(string[] args) { 
+            //Console.NríteLine("HOLA MUNDO!!!");
+            int num1 = 5;
+            int nuero2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+            Console.ReadKey();
+        }
+    }
+}
